1c7b74eaeae339252b76e50fd8d17294c751c89d
renedelcarpio <renedelcarpio@users.noreply.github.com>
2021-02-24 13:35:00 +0000

    deploy: 8536f9121324be608a848314f90e0f7ff74fc364

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,4 +3,49 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const baseUrl = "https://platzi-avo.vercel.app"
+
+const appNode = document.querySelector('#app')
+
+const formatPrice = (price) => {
+    const newPrice = new window.Intl.NumberFormat("en-EN", {
+        style: "currency",
+        currency: "USD"
+    }).format(price)
+
+    return newPrice
+}
+
+//web api
+//Conectarnos al servidor
+window
+    .fetch(`${baseUrl}/api/avo`)
+//procesar la respuesta y convertirla en JSON
+    .then((respuesta) => respuesta.json())
+//JSON -> Data -> Renderizar info en el browser
+    .then ((responseJson) => {
+        const todosLosItems = []
+        responseJson.data.forEach(item => {
+            //crear imagen
+            const imagen = document.createElement("img")
+            //URL de la imagen
+                imagen.src = `${baseUrl}${item.image}`
+                imagen.className = "imagen-palta"
+            //crear título
+            const title = document.createElement("h2")
+                title.textContent = item.name
+                title.className = "nombre-palta"
+            //crear precio
+            const price = document.createElement("div")
+                price.textContent = formatPrice(item.price)
+                price.className = "precio"
+            //contenedor donde pondremos la imagen, el título y el precio
+            const container = document.createElement("div")
+                container.append(imagen, title, price)
+                container.className = "palta"
+
+            todosLosItems.push(container)
+        });
+
+        appNode.append(...todosLosItems)
+    });
